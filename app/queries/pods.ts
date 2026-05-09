cd3e4234a2deed3a48a36925a135dbb5489f6fad
rollokd <rollokd@gmail.com>
2026-05-09 15:06:06 +0100

    feat: pod client api queries with validation

--- app/queries/pods.ts
+++ app/queries/pods.ts
@@ -1,18 +1,34 @@
-import { PodProps } from "@/components/custom/pod";
 import { mutationOptions, queryOptions } from "@tanstack/react-query";
+import { z } from "zod";
+
+const PodSchema = z.object({
+  id: z.string(),
+  x: z.number(),
+  y: z.number(),
+  text: z.string(),
+});
+
+const PodArray = z.array(PodSchema);
+
+export type Pod = z.infer<typeof PodSchema>;
+
+const podKey = () => ["pods"];
+const addPodKey = () => ["addPod"];
+const updatePodKey = (id: string) => ["updatePod", id];
 
 export const podOptions = queryOptions({
-  queryKey: ["pod"],
+  queryKey: podKey(),
   queryFn: async () => {
     const response = await fetch("/api/pods");
 
-    return response.json();
+    const data = await response.json();
+    return PodArray.parse(data);
   },
 });
 
-export const addPodOptions = mutationOptions({
-  mutationKey: ["addPod"],
-  mutationFn: async (pod: PodProps) => {
+export const createPodOptions = mutationOptions({
+  mutationKey: addPodKey(),
+  mutationFn: async (pod: Omit<Pod, "id">) => {
     const response = await fetch("/api/pods", {
       method: "POST",
       headers: {
@@ -23,19 +39,26 @@ export const addPodOptions = mutationOptions({
 
     return response.json();
   },
-});
-
-export const updatePodOptions = mutationOptions({
-  mutationKey: ["updatePod"],
-  mutationFn: async (pod: PodProps) => {
-    const response = await fetch(`/api/pods/${pod.id}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(pod),
-    });
-
-    return response.json();
+  onSuccess: (_data, _variables, _result, context) => {
+    context.client.refetchQueries({ queryKey: podKey() });
   },
 });
+
+export const updatePodOptions = (id: string) =>
+  mutationOptions({
+    mutationKey: updatePodKey(id),
+    mutationFn: async (pod: Pod) => {
+      const response = await fetch(`/api/pods/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pod),
+      });
+
+      return response.json();
+    },
+    onSuccess: (_data, _variables, _result, context) => {
+      context.client.invalidateQueries({ queryKey: podKey() });
+    },
+  });
