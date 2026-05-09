a264a6d74fed4bb502b9aa0303a648bcfd2ab386
rollokd <rollokd@gmail.com>
2026-05-09 15:06:21 +0100

    feat: pod canvas pulling from api and updating

--- components/custom/pod-canvas.tsx
+++ components/custom/pod-canvas.tsx
@@ -1,35 +1,61 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Pod, PodProps } from "@/components/custom/pod";
+import { Pod } from "@/components/custom/pod";
 import { PlusIcon } from "lucide-react";
-import { useState } from "react";
+import { createPodOptions, podOptions } from "@/app/queries/pods";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Spinner } from "../ui/spinner";
+import { getQueryClient } from "@/app/get-query-client";
 
-export function PodCanvas({ pods }: { pods: PodProps[] }) {
-  const [podsState, setPodsState] = useState(pods);
+export function PodCanvas() {
+  const queryClient = getQueryClient();
+  const {
+    data: pods,
+    isPending: isPodsPending,
+    isError,
+  } = useQuery(podOptions);
 
-  function addPod() {
-    setPodsState((prev) => [
-      ...prev,
-      {
-        x: prev.length * 100,
-        y: prev.length * 100,
-        text: "",
-      },
-    ]);
-  }
+  const { mutate, isPending: isCreatingPod } = useMutation(createPodOptions);
 
   return (
     <div className="relative flex flex-1 w-full max-w-3xl flex-col items-center justify-start p-4 sm:items-start">
-      <Button className="w-full" onClick={addPod}>
-        add pod
-        <PlusIcon />
+      <Button
+        className="w-full"
+        onClick={() =>
+          mutate(
+            { x: 0, y: (pods?.length ?? 0) * 100, text: "" },
+            { onSuccess: () => queryClient.refetchQueries(podOptions) },
+          )
+        }
+        disabled={isCreatingPod}
+      >
+        {isCreatingPod ? (
+          <>
+            <Spinner /> adding pod...
+          </>
+        ) : (
+          <>
+            <PlusIcon />
+            add pod
+          </>
+        )}
       </Button>
-      <div className="relative flex w-full h-96">
-        {podsState.map((pod, index) => (
-          <Pod key={index} {...pod} />
-        ))}
-      </div>
+      {isError ? (
+        <div className="relative flex w-full h-96">
+          <p>Error loading pods</p>
+        </div>
+      ) : isPodsPending ? (
+        <div className="relative flex w-full h-96">
+          <Spinner /> loading pods...
+        </div>
+      ) : (
+        <div className="relative flex w-full h-96">
+          {pods.map((pod, index) => (
+            <Pod key={index} {...pod} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- components/custom/pod.tsx
+++ components/custom/pod.tsx
@@ -1,19 +1,19 @@
-import { useId, useState } from "react";
+import { useState } from "react";
 
 export interface PodProps {
+  id: string;
   x: number;
   y: number;
   text: string;
 }
 
-export function Pod({ x, y, text }: PodProps) {
+export function Pod({ id, x, y, text }: PodProps) {
   const [value, setValue] = useState(text);
-  const id = useId();
   return (
     // clear all input styling but add a custom border and allow custom positioning
     <textarea
       id={`pod-${id}`}
-      className="absolute border-red-500 border outline-none field-sizing-content min-w-20 min-h-20 resize"
+      className="absolute border-zinc-500 rounded-md p-1 border outline-none field-sizing-content min-w-20 w-full min-h-20 resize"
       style={{ left: x, top: y }}
       value={value}
       onChange={(e) => setValue(e.target.value)}
