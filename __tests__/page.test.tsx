aa67a4b535ad9e189d7483d2466f4cca7371839f
rollokd <rollokd@gmail.com>
2026-05-10 17:29:48 +0100

    feat: first basic test

---
+++ __tests__/page.test.tsx
@@ -0,0 +1,16 @@
+import { expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Page from "../app/page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+const wrapper = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+test("Page", () => {
+  render(<Page />, { wrapper });
+  expect(
+    screen.getByRole("heading", { level: 1, name: "Quik Pods" }),
+  ).toBeDefined();
+});
